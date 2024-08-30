a9c680a1cbbb9a3711af76e95bc8d0d41875cb1a
Joshua Sirusstara <joshua.sirusstara@binus.edu>
2024-08-31 00:01:47 +0700

    init

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+import os
+from dotenv import load_dotenv
+import bcrypt 
+from fastapi import Depends, FastAPI
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+app = FastAPI()
+load_dotenv()
+security = HTTPBasic()
+
+@app.get("/")
+def init():
+    return os.getenv("PASSWORD")
+
+
+def isAuth(username, password):
+    return ((os.getenv("FASTAPI_USERNAME")==username) and (os.getenv("FASTAPI_PASSWORD")==password))
+
+
+@app.get("/auth")
+def read_current_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    if(not isAuth(credentials.username,credentials.password)):
+       return '500 Not Authorized'
+    return '200 success'
+
+
