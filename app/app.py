32389fff09859d4778922aec8b43d7cfc054df27
Joshua Sirusstara <joshua.sirusstara@binus.edu>
2024-09-03 21:29:02 +0700

    add docs path

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 from typing import Annotated
 import os
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
 
 app = FastAPI(
-    version=0.1,
-    docs_url='/docs',
-    openapi_url='/openapi.json', # This line solved my issue, in my case it was a lambda function
-    openapi_prefix='/pyapi/v1',
-    redoc_url=None
+    version=0.1
+
 )
 load_dotenv()
 security = HTTPBasic()
@@ -28,5 +28,16 @@ def read_current_user(credentials: Annotated[HTTPBasicCredentials, Depends(secur
     if(not isAuth(credentials.username,credentials.password)):
        return '500 Not Authorized'
     return '200 success'
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html(req: Request):
+    root_path = req.scope.get("root_path", "").rstrip("/")
+    openapi_url = root_path + app.openapi_url
+    # print(openapi_url)
+
+    return get_swagger_ui_html(
+        openapi_url=openapi_url,
+        title="API",
+    )
 
 
